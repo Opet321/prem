31fae1251c7e1f72a56221da887baa204337fec0
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-08-20 12:32:53 +0700

    update Ubot/__init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -1,19 +1,9 @@
 import asyncio
 import logging
 import sys
-import time
-import os
+import t
 from datetime import datetime
-from logging.handlers import RotatingFileHandler
-from typing import Any, Dict
-from aiohttp import ClientSession
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from gpytranslate import Translator
-from pyrogram import Client, filters
-from pytgcalls import GroupCallFactory
+from logging.handlers import RotatingFileHandleSession
+from apscheduler.schedulers.asyncio import AsyncIOSche pytgcalls import GroupCallFactory
 
-from config import *
-cmds = None
-CMD_HELP = {}
-clients = []
-ids = []
+from config impo
