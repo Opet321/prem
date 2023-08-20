684fa23816375bdd71896d2e00baeeb54f52bca2
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-08-20 12:35:50 +0700

    update Azazel/__main__.py

--- Azazel/__main__.py
+++ Azazel/__main__.py
@@ -9,58 +9,32 @@ from ubotlibs import *
 from Azazel import BOTLOG_CHATID, aiosession, bot1, bots, app, ids, LOOP, event_loop
 from platform import python_version as py
 from Azazel.logging import LOGGER
-from pyrogram import __version__ as pyro
+fromam import __version__ as pyro
 from Azazel.core.SQL.botlogsql import *
-from Azazel.modules import ALL_MODULES
-from config import SUPPORT, CHANNEL
+from l.modules import ALL_MODULES
+frmport SUPPORTNEL
 import os
-from dotenv import load_dotenv
-
-
-
-MSG_ON = """
-**Azazel Project Actived ✅**
-╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
-◉ **Versi** : `{}`
-◉ **Phython** : `{}`
-◉ **Pyrogram** : `{}`
-**Ketik** `alive`
+from dotenMSG_ON = """
+**Azazel Proje
+╼┅━━━━━━━━━━━━┅╾
+◉ **Versi** : `{}on***Ketik** `alive`
 **untuk Mengecheck Bot**
-╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
-"""
+╼┅━━━━━━━━━━━━━━━━━┅╾
+
 
 
 async def main():
     await app.start()
     LOGGER("Azazel Project").info("Memulai Ubot Pyro..")
-    for all_module in ALL_MODULES:
-        importlib.import_module("Azazel.modules" + all_module)
-    for bot in bots:
-        try:
-            await bot.start()
-            ex = await bot.get_me()
-            user_id = ex.id
-            await ajg(bot)
+    for all_modul ALL_MODULES:
+        importlib.import_module("Azazel.modules" + all_m
+    for bot in bo            await bot.start()
+            ex = await b
+            user_id
+            awaibot)
             await buat_log(bot)
             botlog_group_id = get_botlog(str(user_id))
-            try:
-            	await bot.send_message(botlog_group_id, MSG_ON.format(BOT_VER, py, pyro))
-            except BaseException as a:
-                LOGGER("✓").warning(f"{a}")
-            LOGGER("✓").info("Startup Completed")
-            LOGGER("✓").info(f"Started as {ex.first_name} | {ex.id} ")
-            ids.append(ex.id)
-        except Exception as e:
-            LOGGER("X").info(f"{e}")
-    await idle()
-    await aiosession.close()
-    await app.stop()
-
-
-              
-
-if __name__ == "__main__":
-    LOGGER("Azazel Project").info("Starting  Ubot")
-    install()
-#    LOOP.run_until_complete(main())
-    event_loop.run_until_complete(main())
+            tr
+            	await _group_id, MSG_ON.format(BOT_VER, py, pyro))
+            except Bason as a:
+             ing(f"{a}")
